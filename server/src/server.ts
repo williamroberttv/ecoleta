c89e0bcac5f6f81901ef753adfa9513eba74ce84
willroberttv <willtaavares@gmail.com>
2020-06-07 11:52:23 -0300

    conexão com banco e tabelas criadas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Listagem de Usuários");
-  response.json([{ Hello: "WilliamA", William: "Robert" }]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
