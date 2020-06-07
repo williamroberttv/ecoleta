cb416999d4c88137aeae1a6b7e8a9aaacb445540
willroberttv <willtaavares@gmail.com>
2020-06-07 13:48:05 -0300

    rotas e api finalizadas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import ItemsController from "./controllers/ItemsController";
+import PointsController from "./controllers/PointsController";
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "hello world" });
-});
+routes.get("/items", itemsController.index);
+
+routes.post("/points", pointsController.create);
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
 
 export default routes;
