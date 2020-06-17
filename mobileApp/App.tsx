440c1ecf4bb71033131c787fdb12297a6819d3cb
willroberttv <willtaavares@gmail.com>
2020-06-17 12:28:20 -0300

    pages finalizadas

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading } from "expo";
+import { StatusBar } from "react-native";
+
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+
+import Routes from "./src/routes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
